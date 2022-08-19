93f3d8d9379e00c9b6ea02548df39ddb627ae025
User <glenochieng045@gmail.com>
2022-08-19 10:28:57 +0300

    Styling for welcome page done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import logo from "./logo.svg";
+import blob from "./blob.svg";
+import blob2 from "./blob2.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="top-blob" src={blob} alt="blob"></img>
+      <div className="content-container">
+        <h1 className="title">Quizzical</h1>
+        <p className="description">Test your trivia knowledge</p>
+        <div className="start-quiz">Start Quiz</div>
+      </div>
+      <img className="bottom-blob" src={blob2} alt="blob"></img>
     </div>
   );
 }
