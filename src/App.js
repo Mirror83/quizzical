2a93917379d61eede0d86b18b399961203c9c727
User <glenochieng045@gmail.com>
2022-08-24 08:46:22 +0300

    Partially completed basic styling.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React from "react";
+
+import Questions from "./questions";
+
 import "./App.css";
 
 import logo from "./logo.svg";
@@ -5,17 +9,46 @@ import blob from "./blob.svg";
 import blob2 from "./blob2.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <img className="top-blob" src={blob} alt="blob"></img>
-      <div className="content-container">
-        <h1 className="title">Quizzical</h1>
-        <p className="description">Test your trivia knowledge</p>
-        <div className="start-quiz">Start Quiz</div>
+  const [quizStarted, setQuizStarted] = React.useState(false);
+
+  const questionsAndChoices = Questions.map((quiz) => (
+    <div className="quiz">
+      <div className="question">{quiz.question}</div>
+      <div className="choices">
+        {quiz.choices.map((choice) => (
+          <div className="choice">{choice}</div>
+        ))}
       </div>
-      <img className="bottom-blob" src={blob2} alt="blob"></img>
     </div>
-  );
+  ));
+
+  function startQuiz() {
+    setQuizStarted(true);
+  }
+
+  if (quizStarted) {
+    return (
+      <div className="App">
+        <div className="questions">{questionsAndChoices}</div>
+        <div className="check-answers">Check answers</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <img className="top-blob" src={blob} alt="blob"></img>
+        <div className="content-container">
+          <img className="logo" src={logo} alt="React logo"></img>
+          <h1 className="title">Quizzical</h1>
+          <p className="description">Test your trivia knowledge</p>
+          <div className="start-quiz" onClick={startQuiz}>
+            Start Quiz
+          </div>
+        </div>
+        <img className="bottom-blob" src={blob2} alt="blob"></img>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/questions.js
@@ -0,0 +1,40 @@
+const Questions = [
+  {
+    question: "How does one say goodbye in Spanish?",
+    choices: ["Hola", "Adios", "Au revoir", "Serir"],
+    answer: "Hola",
+    selectedChoice: null,
+  },
+  {
+    question:
+      "Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?",
+    choices: [
+      "Cabbage Patch Kids",
+      "Transformers",
+      "Care Bears",
+      "Rubik's Cube",
+    ],
+    answer: "Care Bears",
+    selectedChoice: null,
+  },
+  {
+    question: "What is the hottest planet in our Solar System?",
+    choices: ["Mercury", "Venus", "Mars", "Jupiter"],
+    answer: "Venus",
+    selectedChoice: null,
+  },
+  {
+    question: "In which country was ceaser salad invented?",
+    choices: ["Italy", "Portugal", "Mexico", "France"],
+    answer: "Mexico",
+    selectedChoice: null,
+  },
+  {
+    question: "How Many Hearts Does An Octopus Have?",
+    choices: ["One", "Two", "Three", "Four"],
+    answer: "Three",
+    selectedChoice: null,
+  },
+];
+
+export default Questions;
